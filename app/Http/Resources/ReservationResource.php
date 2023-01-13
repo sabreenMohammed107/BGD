e24ff8776275b2227e1128d078197d2bf86b1eaa
sabreenMohammed107 <sabreenm312@gmail.com>
2023-01-13 21:27:28 +0200

    test

---
+++ app/Http/Resources/ReservationResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReservationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+
+            'patient' => $this->patient->name ?? '',
+            'clinic' => $this->clinic->name ?? '',
+            'reservation_status' => $this->status->status ?? '',
+
+            'reservation_date' => $this->reservation_date ?? '',
+            'time_from' => $this->time_to ?? '',
+            'time_to' => $this->time_to ?? '',
+            'me / other' => $this->other_flag =1 ?'other' : 'me',
+            'patient_name' => $this->patient_name ?? '',
+            'patient_mobile' => $this->patient_mobile ?? '',
+            'patient_address' => $this->patient_address ?? '',
+        ];
+    }
+}
