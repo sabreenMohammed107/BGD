b58063777f958fb7083dad869e1e6402e56423b5
sabreenMohammed107 <sabreenm312@gmail.com>
2023-04-07 19:39:29 +0200

    price1

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -281,7 +281,7 @@ public function getReservation(Request $request){
         //  }
          if ($max_price) {
 
-            $doctors->where('doctor_clinics.visit_fees','<=',400);
+            $doctors->where('visit_fees','<=',400);
          }
          if ($homeVisit) {
 
@@ -295,14 +295,14 @@ public function getReservation(Request $request){
 
             $doctors=$doctors->where("disability_allowed", $disableAccess);
          }
-         if ($lower) {
-if($lower == 1){
-    $doctors=$doctors->orderby("visit_fees",'Desc');
-}else{
-    $doctors=$doctors->orderby("visit_fees",'asc');
-}
+//          if ($lower) {
+// if($lower == 1){
+//     $doctors=$doctors->orderby("visit_fees",'Desc');
+// }else{
+//     $doctors=$doctors->orderby("visit_fees",'asc');
+// }
 
-         }
+        // }
         $doctors=$doctors->get();
          return $doctors;
             // return $this->sendResponse($doctors, 'All Search result Retrieved  Successfully');
