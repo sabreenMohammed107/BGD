5aef9bc50d9b68f469c068ef86c66ef077597c8b
sabreenMohammed107 <sabreenm312@gmail.com>
2023-04-09 01:40:04 +0200

    kk

--- app/Http/Resources/DoctorClinicResource.php
+++ app/Http/Resources/DoctorClinicResource.php
@@ -17,6 +17,7 @@ class DoctorClinicResource extends JsonResource
         if (App::getLocale() == "en") {
             return [
                 'id'=>$this->id,
+                'clinic_name'=>$this->name,
                 'doctor' => DoctorResource::make($this->doctor),
                 'medical field'=>docFieldsResource::collection($this->doctor->medicField()->get()),
 
@@ -38,7 +39,24 @@ class DoctorClinicResource extends JsonResource
             ];
         }else{
             return [
+                'id'=>$this->id,
+                'clinic_name'=>$this->name,
+                'doctor' => DoctorResource::make($this->doctor),
+                'medical field'=>docFieldsResource::collection($this->doctor->medicField()->get()),
 
+                'phone' => $this->phone ?? '',
+
+                'postal_code' => $this->postal_code ?? '',
+                'google_map' => $this->google_map ?? '',
+                'parking_allowed' => $this->parking_allowed ?? '',
+                'home_visit_allowed' => $this->home_visit_allowed ?? '',
+                'disability_allowed' => $this->disability_allowed ?? '',
+                'clinic_status_id' => $this->clinic_status_id ?? '',
+
+                'avgRating' => round($this->review->avg('stars'),1) ?? '',
+                'reviewCount' => $this->review->count(),
+                'av_day' => $this->next_day ?? '',
+                'av_time' => $this->next_time->time_from ?? '',
                 'street' => $this->dt_street ?? '',
                 'insurance_type' =>$this->insurance->dt_type ?? '',
                 'visit_fees' => $this->insurance_type_id==1 ?  $this->insurance->dt_type : $this->visit_fees  ,
