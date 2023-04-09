f5cb627ddebfcae8ec31f912bb6ce357185e4a0c
sabreenMohammed107 <sabreenm312@gmail.com>
2023-04-10 01:44:19 +0200

    clinic_name

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -250,7 +250,7 @@ public function getReservation(Request $request){
             join('doctors', 'doctor_clinics.doctor_id', '=', 'doctors.id')
             ->join('insurance_types', 'doctor_clinics.insurance_type_id', '=', 'insurance_types.id')
             ->join('doctor_schedules', 'doctor_clinics.id', '=', 'doctor_schedules.clinic_id')
-            ->join('doctor_feilds',  'doctor_feilds.doctor_id','=','doctors.id');
+            ->join('doctor_feilds',  'doctor_feilds.doctor_id','=','doctor_clinics.doctor_id');
 
 
 
