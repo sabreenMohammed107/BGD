0def37e509c9520726672ed2dbc0c546ba6e0bb6
sabreenMohammed107 <sabreenm312@gmail.com>
2023-04-26 19:07:51 +0200

    ff

--- app/Http/Controllers/Api/DoctorsInfController.php
+++ app/Http/Controllers/Api/DoctorsInfController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\BaseController as BaseController;
 use App\Http\Resources\DocorProfileResource;
-use App\Http\Resources\DoctorResource;
 use App\Http\Resources\HomeDoctorResource;
 use App\Http\Resources\MedicalResource;
 use App\Http\Resources\scadualInfoResource;
@@ -33,12 +32,12 @@ class DoctorsInfController extends BaseController
 
         $page['specialists'] = MedicalResource::collection($specialists);
         $doctors = Doctor::with(['medicines'])->take(5)->orderBy("id", "Desc")->get();
-        $newDocotorClinic=Doctor_clinic::take(5)->orderBy("id", "Desc")->get();
+        $newDocotorClinic = Doctor_clinic::take(5)->orderBy("id", "Desc")->get();
         $page['latest_doctors'] = HomeDoctorResource::collection($newDocotorClinic);
         // $page['latest_doctors'] = DoctorResource::collection($doctors);
         //patient - reservation
         // $userid = auth('api')->user()->id;
-        $userid =Auth::user()->id;
+        $userid = Auth::user()->id;
         $current_date = Carbon::now();
         $date = Carbon::parse($current_date)->format('Y-m-d');
         $reservations = Reservation::where('patient_id', $userid)->where('reservation_date', '>=', $date)->whereIn('reservation_status_id', [1, 5])->orderBy("reservation_date", "asc")->get();
@@ -54,54 +53,61 @@ class DoctorsInfController extends BaseController
         $page = [];
         $clinic = Doctor_clinic::where('id', $id)->first();
         $page['clinic_data'] = DocorProfileResource::make($clinic);
-    //    $doctor_schedule=[];
-$rr=[];
+        //    $doctor_schedule=[];
+        $rr = [];
         $schadDys = Doctor_schedule::where('clinic_id', $id)->get();
 
         $start_date = Carbon::now();
         for ($i = 1; $i <= 15; $i++) {
 
             foreach ($schadDys as $schad) {
-                $day =$start_date->dayOfWeek;
-dd($start_date->dayOfWeek);
+                $day = $start_date->dayOfWeek;
+                //order days of week
+                if ($day == 6) {
+                    $day = 1;
+                } else if ($day == 7) {
+                    $day = 2;
+                } else {
+                    $day = $day + 2;
+                }
                 if ($day == $schad->days_id) {
                     // echo  $start_date;
-                    if(App::getLocale()=="en"){
-                    $doctor_schedule[$i] = (object)  [
-                        'date' => $start_date->format('Y-m-d') ,
-                    'day' => $schad->daName->en_day ?? '',
-                    'from' => $schad->time_from,
-                    'to' => $schad->time_to,
-                   ];
-                }else{
-    $doctor_schedule[$i] = (object)  [
-        'date' => $start_date->format('Y-m-d') ,
-    'day' => $schad->daName->dt_day ?? '',
-    'from' => $schad->time_from,
-    'to' => $schad->time_to,
-   ];
-}
+                    if (App::getLocale() == "en") {
+                        $doctor_schedule[$i] = (object) [
+                            'date' => $start_date->format('Y-m-d'),
+                            'day' => $schad->daName->en_day ?? '',
+                            'from' => $schad->time_from,
+                            'to' => $schad->time_to,
+                        ];
+                    } else {
+                        $doctor_schedule[$i] = (object) [
+                            'date' => $start_date->format('Y-m-d'),
+                            'day' => $schad->daName->dt_day ?? '',
+                            'from' => $schad->time_from,
+                            'to' => $schad->time_to,
+                        ];
+                    }
 
                 }
                 // $doctor_schedule['schedule'] = $doctor_schedule;
 
             }
-        //    echo $start_date;
+            //    echo $start_date;
 
-                        $start_date = $start_date->addDays(1);
+            $start_date = $start_date->addDays(1);
 
         }
 
         $page['schedule'] = $doctor_schedule;
 
         //favorite
-        $user=Auth::user();
+        $user = Auth::user();
 
-        if($user){
-            $ff=Favourite_doctor::where('clinic_id',$id)->where('user_id',$user->id)->first();
-            if($ff){
+        if ($user) {
+            $ff = Favourite_doctor::where('clinic_id', $id)->where('user_id', $user->id)->first();
+            if ($ff) {
                 $page['favorite'] = true;
-            }else{
+            } else {
                 $page['favorite'] = false;
             }
 
