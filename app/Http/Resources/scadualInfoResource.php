1357384c0f0dcf4749c6af5d9c1331f1e17606e2
sabreenMohammed107 <sabreenm312@gmail.com>
2023-07-15 23:49:45 +0200

    img

--- app/Http/Resources/scadualInfoResource.php
+++ app/Http/Resources/scadualInfoResource.php
@@ -16,7 +16,7 @@ class scadualInfoResource extends JsonResource
     public function toArray($request)
     {
         $rr=[];
-        // if (App::getLocale() == "en") {
+        if (App::getLocale() == "en") {
 
                 $rr= [
                     'name' => $this->clinic->doctor->name ?? '',
@@ -25,6 +25,8 @@ class scadualInfoResource extends JsonResource
                     'medical field'=>docFieldsResource::collection($this->clinic->doctor->medicField()->get()),
 
                     'image' => asset('uploads/doctors/' . $this->clinic->doctor->img) ?? '',
+                    'address' =>  $this->clinic->en_street ?? '',
+                    'status'=>$this->status->en_status ?? '',
                     'reservation_date' => $this->reservation_date ?? '',
                     'av_day' => $this->reserv_day ?? '',
                     'av_time' =>Carbon::parse($this->time_from)->format('H:i')?? '',
@@ -33,7 +35,22 @@ class scadualInfoResource extends JsonResource
 
 
 
-        // }
+        }else{
+            $rr= [
+                'name' => $this->clinic->doctor->name ?? '',
+
+
+                'medical field'=>docFieldsResource::collection($this->clinic->doctor->medicField()->get()),
+
+                'image' => asset('uploads/doctors/' . $this->clinic->doctor->img) ?? '',
+                'address' =>  $this->clinic->dt_street ?? '',
+                'status'=>$this->status->dt_status ?? '',
+                'reservation_date' => $this->reservation_date ?? '',
+                'av_day' => $this->reserv_day ?? '',
+                'av_time' =>Carbon::parse($this->time_from)->format('H:i')?? '',
+
+            ];
+        }
 return $rr;
     }
 }
