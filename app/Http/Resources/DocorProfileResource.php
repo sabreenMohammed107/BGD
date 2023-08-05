dbbaa02e779324eae43b73554a91e82b4e4554f0
sabreenMohammed107 <sabreenm312@gmail.com>
2023-08-05 23:37:16 +0200

    hii

--- app/Http/Resources/DocorProfileResource.php
+++ app/Http/Resources/DocorProfileResource.php
@@ -37,7 +37,7 @@ class DocorProfileResource extends JsonResource
 
 
 
-                'visit_fees' => $this->insurance_type_id==1 ?  $this->insurance->en_type : $this->visit_fees  ,
+                'visit_fees' => $this->insurance_type_id==1 ?  $this->insurance->en_type : (string)$this->visit_fees  ,
 
                 'avgRating' => round($this->review->avg('stars'),1) ==0 ? 'no patients ratings or reviews yet' : round($this->review->avg('stars'),1),
                 'reviewCount' => $this->review->count()==0 ? '0' : $this->review->count(),
@@ -77,7 +77,7 @@ class DocorProfileResource extends JsonResource
 
 
 
-        'visit_fees' => $this->insurance_type_id==1 ?  $this->insurance->dt_type : $this->visit_fees  ,
+        'visit_fees' => $this->insurance_type_id==1 ?  $this->insurance->dt_type : (string)$this->visit_fees  ,
 
         'avgRating' =>  round($this->review->avg('stars'),1) ==0 ? 'noch keine Patientenbewertungen oder Rezensionen' :  round($this->review->avg('stars'),1),
         'reviewCount' => $this->review->count()==0 ? '0' : $this->review->count(),
--- app/Http/Resources/DoctorClinicResource.php
+++ app/Http/Resources/DoctorClinicResource.php
@@ -62,7 +62,7 @@ class DoctorClinicResource extends JsonResource
                 'av_time' =>Carbon::parse($this->next_time->time_from)->format('H:i')?? '',
                 'street' => $this->dt_street ?? '',
                 'insurance_type' =>$this->insurance->dt_type ?? '',
-                'visit_fees' => $this->insurance_type_id==1 ?  $this->insurance->dt_type : $this->visit_fees  ,
+                'visit_fees' => $this->insurance_type_id==1 ?  $this->insurance->dt_type : (string)$this->visit_fees  ,
 
 
             ];
