94399e0ceb1289236f80f310580554d5be2166ee
sabreenMohammed107 <sabreenm312@gmail.com>
2023-08-11 00:50:38 +0200

    test

--- app/Models/Doctor_clinic.php
+++ app/Models/Doctor_clinic.php
@@ -144,7 +144,11 @@ public function gallery(){
 protected function getActiveGalleryAttribute()
 {
     // return $this->gallery()->where('active','=',1);
-    $array = $this->gallery()->where('active','=',1)
+    // $array = $this->gallery()->where('active','=',1)
+    // ->get()->toArray();
+    $array = $this->gallery()
+    ->join('doctor_clinics','doctor_clinics.id','=','clinic_galleries.clinic_id')
+    ->where('clinic_galleries.active','=',1)
     ->get()->toArray();
 
 // return convertToAssoc($array,'name');
