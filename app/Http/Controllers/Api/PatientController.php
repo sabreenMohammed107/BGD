c8a0b6e8ec91c160f50daeae2d87e39ae6a59a0d
sabreenMohammed107 <sabreenm312@gmail.com>
2023-10-08 00:39:16 +0200

    nextdaty

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -379,9 +379,9 @@ class PatientController extends BaseController
 
             $doctorsMapAfter =$doctors->where('doctor_schedules.days_id' ,'>=',$weekday)
             ->orderBy("doctor_schedules.days_id", 'asc');
-            $doctorsMapBefor = $doctors->where('doctor_schedules.days_id' ,'<',$weekday)
+            $doctorsMapBefor = $doctors->where('doctor_schedules.days_id' ,'<=',$weekday)
             ->orderBy("doctor_schedules.days_id", 'asc');
-dd($weekday);
+
 // foreach($doctorsMapBefor as $ob){
 //       $doctorsMapAfter->push($ob);
 //     // array_push($doctorsMapAfter, $ob);
