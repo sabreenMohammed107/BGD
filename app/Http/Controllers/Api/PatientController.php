1772218fb96de344200fdc2baa5670153e4ca66c
sabreenMohammed107 <sabreenm312@gmail.com>
2023-10-08 09:43:13 +0200

    filter

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -376,8 +376,8 @@ class PatientController extends BaseController
                 $weekday = $weekMap[$dayOfTheWeek];
 
                 $doctorsMapAfter = $doctors
-                ->orderBy("doctor_schedules.days_id", 'asc')
-                ->orderByRaw(\DB::raw("FIELD(doctor_schedules.days_id,$weekday)"))
+                // ->orderBy("doctor_schedules.days_id", 'asc')
+                ->orderByRaw(\DB::raw("FIELD(doctor_schedules.days_id,$weekday), asc"))
                ;
 
 
