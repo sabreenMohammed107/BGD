7aa0f3ba52b21341aaa2248b53718eb09b4e0f74
sabreenMohammed107 <sabreenm312@gmail.com>
2023-10-10 14:19:53 +0200

    tt

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -376,11 +376,11 @@ class PatientController extends BaseController
 
                 $dayOfTheWeek = Carbon::now()->dayOfWeek;
                 $dFake = $weekMap[$dayOfTheWeek];
-dd($dFake);
+
                 $doctorsMapAfter = $doctors->where('doctor_schedules.days_id', '>=', $dFake)
-                    ->orderBy("doctor_schedules.days_id", 'asc')->groupBy('doctor_clinics.id')->get();
+                    ->orderBy("doctor_schedules.days_id", 'asc')->get();
                 $doctorsMapBefore = $doctors->where('doctor_schedules.days_id', '<', $dFake)
-                    ->orderBy("doctor_schedules.days_id", 'desc')->groupBy('doctor_clinics.id')->get();
+                    ->orderBy("doctor_schedules.days_id", 'desc')->get();
 
                    //get days
                 $doctors = $doctorsMapAfter->toBase()->merge($doctorsMapBefore);
