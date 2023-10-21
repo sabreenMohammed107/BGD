500a38e1ac9aa78cbc1c35cc05e50f46e7bee05b
sabreenMohammed107 <sabreenm312@gmail.com>
2023-10-21 14:59:17 +0200

    new test

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -407,9 +407,9 @@ class PatientController extends BaseController
                 // return $this->sendResponse(DoctorClinicResource::collection($doctors), 'All Search result Retrieved  Successfully');
 // new testing
 $scadsaft=Doctor_schedule::where('days_id', '>=', $dFake) ->orderBy("days_id", 'asc')->pluck('id');
-$scadsbef=Doctor_schedule::where('days_id', '<=', $dFake) ->orderBy("days_id", 'asc')->pluck('id');
+$scadsbef=Doctor_schedule::where('days_id', '<', $dFake) ->orderBy("days_id", 'asc')->pluck('id');
 $doctorsTest= $scadsaft->merge($scadsbef);
-dd([ $dayOfTheWeek,$dFake,$scadsaft,$scadsbef]);
+dd([$doctorsTest]);
 $doctors = $doctors->whereIn("doctor_schedules.id", $doctorsTest)->get();
 return $this->sendResponse(DoctorClinicResource::collection($doctors), 'All Search result Retrieved  Successfully');
             } else {
