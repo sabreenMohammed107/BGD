1a73f983f714eeb75a7d221f86dfee8687b79532
sabreenMohammed107 <sabreenm312@gmail.com>
2023-10-25 16:43:50 +0200

    test otp

--- app/Services/OtpService.php
+++ app/Services/OtpService.php
@@ -17,7 +17,7 @@ class OtpService
             'to' => $phoneNumber, //Receiver (required)
             'text' => $otp, //Message (required)
 
-            'from' => "BDG-App" //Sender (required)
+            'from' => 'sender' //Sender (required)
         ];
 
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
