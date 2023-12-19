803c5caed19ad38283355ab79155098fa95df2c9
sabreen mohammed <sabreen@wedomore.com>
2023-12-19 13:33:59 +0200

    test status

--- resources/views/admin/reservation/all.blade.php
+++ resources/views/admin/reservation/all.blade.php
@@ -25,6 +25,48 @@
 @endsection
 
 @section('content')
+<style>
+    .dropbtn {
+      background-color: #3498DB;
+      color: white;
+      padding: 16px;
+      font-size: 16px;
+      border: none;
+      cursor: pointer;
+    }
+
+    .dropbtn:hover, .dropbtn:focus {
+      background-color: #2980B9;
+    }
+
+    .dropdown {
+         position: inherit !important;
+         display: block;
+
+
+    }
+
+    .dropdown-content {
+      display: none;
+      /* position: relative; */
+      background-color: #f6f6f6;
+      min-width: 120px;
+      overflow: auto;
+
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      z-index: 9999999;
+    }
+
+    .dropdown-content a {
+      padding: 5px 16px;
+      text-decoration: none;
+      display: block;
+    }
+
+    .dropdown a:hover {background-color: #ddd;}
+
+    .show {display: block;}
+    </style>
     <!--begin::Post-->
     <div class="post fs-6 d-flex flex-column-fluid" id="kt_post">
         <!--begin::Container-->
@@ -110,6 +152,25 @@
 @endsection
 @section('scripts')
     <script>
+        /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
         $('#search-form').on('submit', function(e) {
             var name = $('#testCheck').val();
             if (name == "admin") {
@@ -154,6 +215,8 @@
                             }, // Disable ordering on column 3 (actions)
                         ]
                     });
+                    // $(".dropdown-toggle").dropdown();
+                    $('.menu-sub-dropdown').get('menu');
                     const filterSearch = document.querySelector(
                         '[data-kt-ecommerce-category-filter="search"]');
                     filterSearch.addEventListener('keyup', function(e) {
