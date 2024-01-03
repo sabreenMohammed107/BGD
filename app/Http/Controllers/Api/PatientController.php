b47277ef03dab9aad87d316b388bdc4363d61f44
sabreen mohammed <sabreen@wedomore.com>
2024-01-03 16:32:02 +0200

    test merge

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -493,29 +493,19 @@ class PatientController extends BaseController
                 //     ->join('doctor_schedules', 'doctor_clinics.id', '=', 'doctor_schedules.clinic_id')
                 //     ->join('doctor_feilds', 'doctor_feilds.doctor_id', '=', 'doctor_clinics.doctor_id')
                 //     ->whereIn("doctor_schedules.id", $doctorsTest)->orderBy("doctor_schedules.days_id", 'asc')->groupBy('doctor_clinics.id')->get();
-
-                $doctorsBefore = Doctor_clinic::select('doctor_clinics.*')
-                    ->join('doctors', 'doctor_clinics.doctor_id', '=', 'doctors.id')
-                    ->join('insurance_types', 'doctor_clinics.insurance_type_id', '=', 'insurance_types.id')
-                    ->join('doctor_schedules', 'doctor_clinics.id', '=', 'doctor_schedules.clinic_id')
-                    ->join('doctor_feilds', 'doctor_feilds.doctor_id', '=', 'doctor_clinics.doctor_id')
-                    ->where("doctor_schedules.days_id", "<", $dFake)
+                $doctorsBeforeIds =$doctors
+                ->where("doctor_schedules.days_id", "<", $dFake)
                     ->orderBy("doctor_schedules.days_id", 'asc')
-                    ->get();
+                    ->pluck("doctor_clinics.id");
 
-                $doctorsAfter = Doctor_clinic::select('doctor_clinics.*')
-                    ->join('doctors', 'doctor_clinics.doctor_id', '=', 'doctors.id')
-                    ->join('insurance_types', 'doctor_clinics.insurance_type_id', '=', 'insurance_types.id')
-                    ->join('doctor_schedules', 'doctor_clinics.id', '=', 'doctor_schedules.clinic_id')
-                    ->join('doctor_feilds', 'doctor_feilds.doctor_id', '=', 'doctor_clinics.doctor_id')
-                    ->where("doctor_schedules.days_id", ">=", $dFake)
+                $doctorsAfterIds =$doctors->where("doctor_schedules.days_id", ">=", $dFake)
                     ->orderBy("doctor_schedules.days_id", 'asc')
-                    ->get();
-
+                    ->pluck("doctor_clinics.id");
+dd($doctorsBeforeIds);
                     //sabreen
                 $mergedDoctors = $doctorsAfter->merge($doctorsBefore);
                 // ->unique('id')->values();
-dd($doctorsBefore);
+
                 $uniqueDoctors = collect([]);
 
                 foreach ($mergedDoctors as $doctor) {
